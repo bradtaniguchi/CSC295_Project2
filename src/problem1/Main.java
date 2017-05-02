985b001657599700779ea1dafcf03d1ac491b0ed
bradtaniguchi <bradtaniguchi@gmail.com>
2017-05-01 21:34:38 -0700

    First commit

---
+++ src/problem1/Main.java
@@ -0,0 +1,14 @@
+package problem1;
+
+/**
+ *
+ * @author Rudy Leiva
+ * @author Bradley Taniguchi
+ * 5/1/17
+ * CSC 295
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Problem 1");
+    }
+}
---
+++ src/problem2/Main.java
@@ -0,0 +1,14 @@
+package problem2;
+
+/**
+ *
+ * @author Rudy Leiva
+ * @author Bradley Taniguchi
+ * 5/1/17
+ * CSC 295
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Problem 2");
+    }
+}
