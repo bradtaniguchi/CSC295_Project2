4e009ac928c99e2b98d2a8ec2242541bba750a41
Brad <bradtaniguchi@gmail.com>
2017-05-18 12:41:21 -0700

    Fixed random generation issue
    There still is the question of if the bounds and odds are correct.

--- src/problem2/SpecialNumbers.java
+++ src/problem2/SpecialNumbers.java
@@ -12,15 +12,13 @@ public class SpecialNumbers {
     private int nonPrimeNum;
     private int primeNum;
     private int perfectNum;
-    private Random random = new Random();
+    private final Random random = new Random();
+
     public SpecialNumbers() {
         this.nonPrimeNum = random.nextInt(10000) + 1; //i thinks this is right..
         this.primeNum = random.nextInt(10000) + 1;
         this.perfectNum = weightedRandom();
     }
-    /**
-     * For Debugging constructor
-     */
     public SpecialNumbers(int nonPrimeNum, int primeNum, int perfectNum) {
         this.nonPrimeNum = nonPrimeNum;
         this.primeNum = primeNum;
@@ -34,17 +32,26 @@ public class SpecialNumbers {
         /*get a number between 1 and 10 for temp*/
         int temp = random.nextInt(10) + 1; 
         int num; //this will be the value we return
-        if(temp >= 3) {
+        if(temp <= 3) {
             num = random.nextInt(50) + 1;
-        } else if (temp >= 8) {
+            return num;
+        } else if (temp <= 8) {
             num = random.nextInt(400) + 51;
-        } else if (temp >= 9 ) {
+            return num;
+        } else if (temp <= 10 ) {
             num = random.nextInt(50) + 451;
+            return num;
         } else { //do this incase the random generation is wrong
             System.out.println("ERROR in Weighted Random! " + temp);
             num = -1;
         }
         return num;
+        
+        /*
+        nstructor, initialize nonPrimeNum and primeNum to two random numbers
+        within [1, 10000], and the probablities for primeNum to be a random number within the range [1, 50], [50, 450] and
+        [451, 500] are 30%, 50%, and 20%, respectively.
+        */
     }
     /*getters and setters*/
     public int getNonPrimeNum() {
