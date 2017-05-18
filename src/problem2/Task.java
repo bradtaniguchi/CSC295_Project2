f8921a10fb1866e61000add2913e3da32c7f62d7
Brad <bradtaniguchi@gmail.com>
2017-05-18 11:54:06 -0700

    Fixed indentation, and javadoc notation
    Cmon rudy, your graduating, you know how to indent correctly XD

--- src/problem2/Task.java
+++ src/problem2/Task.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package problem2;
 
 /**
@@ -15,38 +10,46 @@ package problem2;
 public class Task {
     
     
-    //checks whether an int is prime or not.
-boolean isPrime(int n)
-{
-    //check if n is a multiple of 2
-    if (n%2==0) return false;
-    //if not, then just check the odds
-    for(int i=3;i*i<=n;i+=2) {
-        if(n%i==0)
-            return false;
-    }
-    return true;
-}
-
-//checks whether an int is perfect or not
-boolean isPerfect(int n)
-{
-    int sum = 0;
-    
-    for(int i = 1; i < n; i++)
+    /**
+     * checks whether an integer is prime or not.
+     * @param n the number to check
+     * @return if the number is prime or not.
+     */
+    boolean isPrime(int n)
     {
-        if(n % i == 0)
-        {
-            sum += i;
+        //check if n is a multiple of 2
+        if (n%2==0) return false;
+        //if not, then just check the odds
+        for(int i=3;i*i<=n;i+=2) {
+            if(n%i==0)
+                return false;
         }
-    }
-    if(sum == n)
-    {
         return true;
     }
-    else
+
+    /**
+     * checks whether an integer is perfect or not.
+     * @param n the number to check
+     * @return  if the number is perfect or not.
+     */
+    boolean isPerfect(int n)
     {
-        return false;
+        int sum = 0;
+
+        for(int i = 1; i < n; i++)
+        {
+            if(n % i == 0)
+            {
+                sum += i;
+            }
+        }
+        if(sum == n)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
-}
 }
