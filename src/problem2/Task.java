f75775a809362100cbc8b0f2332a45ca4fb08304
rudyleiva93 <rudyleiva93@yahoo.com>
2017-05-18 10:56:59 -0700

    Added task class & 'run' method to worker class

---
+++ src/problem2/Task.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problem2;
+
+/**
+ *
+ * @author rleiva
+ */
+public class Task {
+    
+    
+    //checks whether an int is prime or not.
+boolean isPrime(int n)
+{
+    //check if n is a multiple of 2
+    if (n%2==0) return false;
+    //if not, then just check the odds
+    for(int i=3;i*i<=n;i+=2) {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+//checks whether an int is perfect or not
+boolean isPerfect(int n)
+{
+    int sum = 0;
+    
+    for(int i = 1; i < n; i++)
+    {
+        if(n % i == 0)
+        {
+            sum += i;
+        }
+    }
+    if(sum == n)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+}
--- src/problem2/Worker.java
+++ src/problem2/Worker.java
@@ -1,6 +1,6 @@
 package problem2;
 
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  *
@@ -9,9 +9,24 @@ import java.util.concurrent.LinkedBlockingQueue;
  * 5/1/17
  * CSC 295
  */
-public class Worker extends Thread {
-    private LinkedBlockingQueue<SpecialNumbers> jobs;
-    public Worker(LinkedBlockingQueue<SpecialNumbers> jobs) {
-        this.jobs = jobs;
+public class Worker extends Thread 
+    {
+        private ConcurrentLinkedQueue<Task> taskQueue;
+        
+        public Worker(ConcurrentLinkedQueue<Task> taskQueue) {
+        this.taskQueue = taskQueue;
+    }
+    
+    public void run()
+    {
+       while(true)
+       {
+           Task task = taskQueue.poll();
+           if(task == null)
+           {
+               break;
+           }
+           //task.weightedRandom();
+       }
     }
 }
